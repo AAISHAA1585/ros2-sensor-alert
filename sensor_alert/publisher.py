2169cdb7f82bae4fce9c8638c23c3c70e6ef4ae6
AAISHAA1585 <aishwaryasuryawanshi2021@gmail.com>
2025-08-15 22:16:45 +0530

    Basic ROS2 sensor alert project

---
+++ sensor_alert/publisher.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+import random
+
+class SensorPublisher(Node):
+    def __init__(self):
+        super().__init__('sensor_publisher')
+        self.publisher_ = self.create_publisher(Float32, 'sensor_data', 10)
+        self.timer = self.create_timer(1.0, self.publish_data)
+
+    def publish_data(self):
+        value = random.uniform(0, 100)
+        msg = Float32()
+        msg.data = value
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: {value:.2f}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SensorPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'sensor_alert'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aishwarya',
+    maintainer_email='aishwarya@example.com',
+    description='A simple ROS2 package for temperature alerts',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sensor_pub = sensor_alert.publisher:main',
+            'sensor_sub = sensor_alert.subscriber:main',
+        ],
+    },
+)
+
